7cd3dafa15f0d62591d7c5e9dcfbebc33aaaf596
Vithushan Logeswaran <2018csc042@univ.jfn.ac.lk>
2023-06-10 10:57:33 +0530

    Initial Commit

---
+++ Firebase Connection/Firebase_ConnectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_ConnectionApp.swift
+//  Firebase Connection
+//
+//  Created by Vithushan Logan on 2023-06-10.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_ConnectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
